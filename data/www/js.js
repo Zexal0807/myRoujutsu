496577994b84558f1ead6e60dc3f3afc88e12e69
Zexal0807 <gallinar00@gmail.com>
2020-03-06 14:50:58 +0100

    Add firebase

--- data/www/js.js
+++ data/www/js.js
@@ -1,8 +1,12 @@
 document.addEventListener('deviceready', function () {
+	
+	window.plugins.PushbotsPlugin.initialize("5e6250782ce1df73b63e6263", {"android":{"sender_id":"430881925689"}});
+	
+	
     alert(cordova.plugins.notification.local.launchDetails);
 	
 	cordova.plugins.notification.local.schedule({
-		id: 2,
+		id: 1,
     	title: 'My first notification',
     	text: 'Thats pretty easy...'
 	});
