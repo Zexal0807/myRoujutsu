f2c4d256eba27395f188a02b8c2524bca29572c3
Zexal0807 <gallinar00@gmail.com>
2020-03-10 10:41:58 +0100

    Add b

--- data/www/js.js
+++ data/www/js.js
@@ -1,28 +1,113 @@
+var pushNotification;
+
 document.addEventListener('deviceready', function () {
-	
-	FCMPlugin.onTokenRefresh(function(token){
-		alert( token );
-	});
-	FCMPlugin.getToken(function(token){
-		alert(token);
-	});
-	FCMPlugin.subscribeToTopic('topicExample');
-	FCMPlugin.onNotification(function(data){
-		if(data.wasTapped){
-		  //Notification was received on device tray and tapped by the user.
-		  alert( JSON.stringify(data) );
-		}else{
-		  //Notification was received in foreground. Maybe the user needs to be notified.
-		  alert( JSON.stringify(data) );
-		}
-	});	
-	
-	
+	pushNotification = window.plugins.pushNotification;
+
+	if (device.platform == 'android' || device.platform == 'Android' || device.platform == "amazon-fireos") {
+		pushNotification.register(
+			successHandler,
+			errorHandler,
+			{
+				"senderID": "430881925689d",
+				"ecb": "onNotification"
+			});
+	} else if (device.platform == 'blackberry10') {
+		pushNotification.register(
+			successHandler,
+			errorHandler,
+			{
+				invokeTargetId: "replace_with_invoke_target_id",
+				appId: "replace_with_app_id",
+				ppgUrl: "replace_with_ppg_url", //remove for BES pushes
+				ecb: "pushNotificationHandler",
+				simChangeCallback: replace_with_simChange_callback,
+				pushTransportReadyCallback: replace_with_pushTransportReady_callback,
+				launchApplicationOnPush: true
+			});
+	} else {
+		pushNotification.register(
+			tokenHandler,
+			errorHandler,
+			{
+				"badge": "true",
+				"sound": "true",
+				"alert": "true",
+				"ecb": "onNotificationAPN"
+			});
+	}
+
 }, false);
-function sw(){
+
+function successHandler (result) {
+    alert('result = ' + result);
+}
+
+function errorHandler (error) {
+    alert('error = ' + error);
+}
+
+function onNotification(e) {
+    $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+ 
+    switch( e.event )
+    {
+    case 'registered':
+        if ( e.regid.length > 0 )
+        {
+            $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+            // Your GCM push server needs to know the regID before it can push to this device
+            // here is where you might want to send it the regID for later use.
+            console.log("regID = " + e.regid);
+        }
+    break;
+ 
+    case 'message':
+        // if this flag is set, this notification happened while we were in the foreground.
+        // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+        if ( e.foreground )
+        {
+            $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+ 
+            // on Android soundname is outside the payload.
+            // On Amazon FireOS all custom attributes are contained within payload
+            var soundfile = e.soundname || e.payload.sound;
+            // if the notification contains a soundname, play it.
+            var my_media = new Media("/android_asset/www/"+ soundfile);
+            my_media.play();
+        }
+        else
+        {  // otherwise we were launched because the user touched a notification in the notification tray.
+            if ( e.coldstart )
+            {
+                $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+            }
+            else
+            {
+                $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+            }
+        }
+ 
+       $("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+           //Only works for GCM
+       $("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+       //Only works on Amazon Fire OS
+       $status.append('<li>MESSAGE -> TIME: ' + e.payload.timeStamp + '</li>');
+    break;
+ 
+    case 'error':
+        $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+    break;
+ 
+    default:
+        $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+    break;
+  }
+}
+
+function sw() {
 	cordova.plugins.notification.local.schedule({
 		id: 2,
-    	title: 'My first notification',
-    	text: 'Thats pretty easy...'
+		title: 'My first notification',
+		text: 'Thats pretty easy...'
 	});
 }
